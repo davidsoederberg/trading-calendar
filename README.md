09ba194be29a9e1478d53e064aeb4ea62cc020b0
David Söderberg <31454554+davidsoederberg@users.noreply.github.com>
2021-01-18 15:13:11 +0100

    Fix: Restructure (#9)
    * semantic-release to bump package.json version
    
    * oslo 2021
    
    * new-york 2021
    
    * genric tests
    
    * Better error messages
    
    * Error if year is unsupported

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Trading Calendar
 =======================================
 
-![Test and Release](https://github.com/davidsoederberg/trading-calendar/workflows/Test%20and%20Release/badge.svg)
+![Build](https://github.com/davidsoederberg/trading-calendar/workflows/Build/badge.svg)
 ![npm](https://img.shields.io/npm/v/trading-calendar)
 ![licence](https://img.shields.io/npm/l/trading-calendar)
 
@@ -17,7 +17,9 @@ Feel free to contribute with adding more exchanges and years.
 
 ## Supported exchanges
 
-* Stockholm
+* new-york
+* stockholm
+* oslo
 
 ## Supported years
 
--- src/exchange.ts
+++ src/exchange.ts
@@ -13,67 +13,68 @@ class Exchange {
   constructor(name: string) {
     this.name = name;
     const exchangeInfo = exchanges[name as keyof typeof exchanges];
-    if(!exchangeInfo) throw new Error('Incorrect exchange name or exchange not available. Check the docs for available exchanges. If you would like to add an exchange, create an issue on the github page');
+    if(!exchangeInfo) throw new Error('Incorrect exchange name or exchange not supported. Check the docs for supported exchanges. If you would like to add an exchange, create an issue on the github page');
     this.timezone = exchangeInfo.timezone;
     this.openTime = exchangeInfo.openTime;
     this.closeTime = exchangeInfo.closeTime;
     this.calendar = exchangeInfo.calendar;
   }
   isTradingToday(): boolean {
-    return this.isTradingDayDateTime(DateTime.local().setZone(this.timezone));
+    return this.checkIfTradingDay(DateTime.local().setZone(this.timezone));
   }
   isTradingDay(date: string | Date | DateTime): boolean {
     if(typeof date === 'string') {
       const reg = /^(19|20)\d\d[- \/.](0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])$/;
       if(reg.test(date)) {
-        return this.isTradingDayDateTime(DateTime.fromFormat(date, 'yyyy-mm-dd'))
+        return this.checkIfTradingDay(DateTime.fromFormat(date, 'yyyy-mm-dd'))
       } else {
         throw new Error(`Incorrect format on ${date}. The correct format is yyyy-mm-dd, for example 2021-08-11`);
       }
     }
     if(date instanceof Date) {
-      return this.isTradingDayDateTime(DateTime.fromJSDate(date));
+      return this.checkIfTradingDay(DateTime.fromJSDate(date));
     }
     if(date instanceof DateTime) {
-      return this.isTradingDayDateTime(date);
+      return this.checkIfTradingDay(date);
     }
-    throw new Error('Error');
+    throw new Error(`Date:${date} must be of type string, Date or DateTime.`);
   }
-  private isTradingDayDateTime(localTime: DateTime) {
+  private checkIfTradingDay(localTime: DateTime) {
     if (localTime.weekday > 5) return false;
     const months = this.calendar[localTime.year];
+    if(!months) throw new Error(`${localTime.year} is unsupported. Feel free to add more support with creating an issue on the github page.`);
     if (months.hasOwnProperty(localTime.monthLong)) {
       return !months[localTime.monthLong].includes(localTime.day);
     }
     return true;
   }
+  isTradingNow(): boolean {
+    return this.checkIfTradingTime(DateTime.local().setZone(this.timezone));
+  }
   isTradingTime(date: string | Date | DateTime): boolean {
     if(typeof date === 'string') {
       const reg = /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])[-]([01][0-9]|2[0123])[:]([0-5][0-9])$/;
       if(reg.test(date)) {
-        return this.isTradingTimeDateTime(DateTime.fromFormat(date, 'yyyy-MM-dd-HH:mm'));
+        return this.checkIfTradingTime(DateTime.fromFormat(date, 'yyyy-MM-dd-HH:mm'));
       } else {
-        throw new Error('Incorrect format on ${date}. The corrrect format is yyyy-mm-dd-HH24-MI, for example 2021-08-11-20:30');
+        throw new Error(`Incorrect format on ${date}. The corrrect format is yyyy-mm-dd-HH24-MI, for example 2021-08-11-20:30`);
       }
     }
     if(date instanceof Date) {
-      return this.isTradingTimeDateTime(DateTime.fromJSDate(date));
+      return this.checkIfTradingTime(DateTime.fromJSDate(date));
     }
     if(date instanceof DateTime) {
-      return this.isTradingTimeDateTime(date);
+      return this.checkIfTradingTime(date);
     }
-    throw new Error('Error');
+    throw new Error(`Date:${date} must be of type string, Date or DateTime.`);
   }
-  private isTradingTimeDateTime(localTime: DateTime) {
-    if (!this.isTradingDayDateTime(localTime)) return false;
+  private checkIfTradingTime(localTime: DateTime) {
+    if (!this.checkIfTradingDay(localTime)) return false;
     const hour = localTime.hour;
     const minute = localTime.minute;
     return (hour > this.openTime.hour && hour < this.closeTime.hour)
     || (hour === this.openTime.hour && minute >= this.openTime.minute)
     || (hour === this.closeTime.hour && minute < this.closeTime.minute);
-  }
-  isTradingNow(): boolean {
-    return this.isTradingTimeDateTime(DateTime.local().setZone(this.timezone));
   }
 }
 
